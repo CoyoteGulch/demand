852a16a5c07b55d035f0b3eb19a10246d2139860
CoyoteGulch <coyotegulch@mac.com>
2022-06-21 12:41:22 -0600

    Fixed the problem of translating month_day from character to date. Plot is working but needs polish. Program runs.

--- bars_with_lines_raw_water_2010_to_2021.R
+++ bars_with_lines_raw_water_2010_to_2021.R
@@ -18,12 +18,12 @@ dfrwfinaltibble <- tibble(dfrwfinaltibble)
 dfrwfinaltibble <- dfrwfinaltibble %>% pivot_longer(c(y_2010, y_2011, y_2012, y_2013, y_2014, y_2015, y_2016, y_2017, y_2018, y_2019, y_2020, y_2021), names_to = "year", values_to = "acre_feet" )
 #
 # Change the month_day column adding the year so that ggplot can treat it as a date
+dfrwfinaltibble$month_day <- paste(str_sub(dfrwfinaltibble$year, 3, 6), "-", dfrwfinaltibble$month_day, sep = "")
+dfrwfinaltibble$month_day <- as.Date(dfrwfinaltibble$month_day)
+#
+# Remove the year column
 dfrwfinaltibble <- subset(dfrwfinaltibble, select = -year)
-dfrwfinaltibble$month_day <- as.Date(as.character(paste(dfrwfinaltibble$month_day, str_sub(dfrwfinaltibble$year, 3, 6))))
-
-#dfrwfinaltibble$Month_Day <- as.Date(paste("2021-", dfinal21$Month_Day, sep = ""))
-
-#dfrwfinaltibble$month_day <- as.Date(as.character(dfrwfinaltibble$month_day), "%y/%m/%d")
+#
 # Used during testing
 View(dfrwfinaltibble)
 #
@@ -31,6 +31,6 @@ View(dfrwfinaltibble)
 p <- ggplot() + geom_col(data = dfrwfinaltibble, aes(y = acre_feet, x = month_day), color = "darkblue", fill = "white") +
   coord_cartesian(ylim = c(20,155)) +
   scale_x_date(breaks = "1 month", minor_breaks = "1 day", labels=date_format("%b")) +
-  labs(x = "Month", y = "acre-feet", title = "Treatment Plant Draw on Water Water 2010 - 2021")
+  labs(x = "Year", y = "acre-feet", title = "Treatment Plant Draw on Water Water 2010 - 2021")
 print(p)
 #
