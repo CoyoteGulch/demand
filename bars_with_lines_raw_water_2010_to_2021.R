e0e933accf855df976d5c055744b35a3e907f2b3
CoyoteGulch <coyotegulch@mac.com>
2022-07-14 12:59:57 -0600

    program now plots daily 12 year means line with a line for daily totals. Program runs.

--- bars_with_lines_raw_water_2010_to_2021.R
+++ bars_with_lines_raw_water_2010_to_2021.R
@@ -19,29 +19,29 @@ dfrwfinaltibble[60, 2:13] <- na_if(dfrwfinaltibble[60, 2:13], 0)
 #
 # Calculate the means for each row and create a new column called 12_year_mean
 # change the column names to something more meaningful
-dfrwfinaltibble <- cbind(dfrwfinaltibble, rowMeans(dfrwfinaltibble[, 2:13], na.rm = TRUE))
-dfrwfinaltibble <- rename(dfrwfinaltibble, "12_year_mean" = "rowMeans(dfrwfinaltibble[, 2:13], na.rm = TRUE)")
+#dfrwfinaltibble <- cbind(dfrwfinaltibble, rowMeans(dfrwfinaltibble[, 2:13], na.rm = TRUE))
+#dfrwfinaltibble <- rename(dfrwfinaltibble, "12_year_mean" = "rowMeans(dfrwfinaltibble[, 2:13], na.rm = TRUE)")
 #
-
-#### START HERE. DETERMINE HOW TO PIVOT_LONGER INTO 2 COLUMNS
-
 # Tidy dfrwfinaltibble
-dfrwfinaltibble <- dfrwfinaltibble %>% pivot_longer(c(y_2010, y_2011, y_2012, y_2013, y_2014, y_2015, y_2016, y_2017, y_2018, y_2019, y_2020, y_2021), names_to = "year", values_to = "acre_feet")
+dfrwfinaltibble <- dfrwfinaltibble %>%pivot_longer(c(y_2010, y_2011, y_2012, y_2013, y_2014, y_2015, y_2016, y_2017, y_2018, y_2019, y_2020, y_2021), names_to = "year", values_to = "acre_feet")
 #
 # Change the month_day column adding the year so that ggplot can treat it as a date
 dfrwfinaltibble$month_day <- paste(str_sub(dfrwfinaltibble$year, 3, 6), "-", dfrwfinaltibble$month_day, sep = "")
 dfrwfinaltibble$month_day <- as.Date(dfrwfinaltibble$month_day)
 #
 # Remove the year column
-dfrwfinaltibble <- subset(dfrwfinaltibble, select = -year)
+# dfrwfinaltibble <- subset(dfrwfinaltibble, select = -year)
 #
 # Used during testing
 View(dfrwfinaltibble)
-
-
 #
-
-
+# Create a tibble to hold the daily 12 year means
+dfrwfinaltibblemeans <- tibble(dfrwfinal)
+# Replace zeros in row 60 with NAs since February 29th occurs only in leap years
+dfrwfinaltibblemeans[60, 2:13] <- na_if(dfrwfinaltibblemeans[60, 2:13], 0)
+# Load the 12 year means in each year column
+dfrwfinaltibblemeans[, 2:13] <- rowMeans(dfrwfinaltibblemeans[, 2:13], na.rm = TRUE)
+# Tidy dfrwfinaltibblemeans
 #
 # Plot the data
 coeff <- 325851.4
